28d8c9b6bd1030d32435d5a623392a590b7ebab0
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-04 21:50:14 +0100

    refactored unit tests, and added 2nd item to functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Check out Home page
         self.browser.get("http://localhost:8000")
@@ -31,16 +36,15 @@ class NewVisitorTest(unittest.TestCase):
         # User hits enter, the page updates and "1: go shopping" is an item on the lists
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Go shopping" for row in rows),
-            "new to-do item does not appear in table"
-        )
         # A text box to add another item is still present
         # User adds "2: Remember to pick up dry-cleaning" and enters
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Go to bank")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # Page updates again and both items appear on list
+        self.check_for_row_in_list_table("1: Go shopping")
+        self.check_for_row_in_list_table("2: Go to bank")
         # Website generates a unique url for the list, it is explained
         # Once URL is visted, the list is there
         self.fail("Finish the test!")
