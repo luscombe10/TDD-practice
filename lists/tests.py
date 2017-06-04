b921e08ddc978b75334682e77c83eeb5fa44d3b9
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-04 15:34:32 +0100

    Basic view returns minmal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
-
+from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
@@ -9,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resovles_to_home_page(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
