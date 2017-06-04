6488769a4638e1dbe7d7b5765aeea7e935eb9148
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-04 18:15:18 +0100

    Functional tests now check we can input at to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
+import time
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,17 +16,32 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Check out Home page
         self.browser.get("http://localhost:8000")
-        #look for correct page title
+        # look for correct page title and header
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
-    # User is prompted to input item straight away
-    # User Types "1: go shopping" into the prompt
-    # User hits enter, the page updates and "1: go shopping is an item on the list
-    # A text box to add another item is still present
-    # User adds "2: Remember to pick up dry-cleaning" and enters
-    # Page updates again and both items appear on list
-    # Website generates a unique url for the list, it is explained
-    # Once URL is visted, the list is there
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
+        # User is prompted to input item straight away
+        inputbox = self.browser.find_element_by_tag_name("id_new_item")
+        self.assertEqual(
+            inputbox.get_attributre("placeholder"),
+            "Enter a to-do item"
+        )
+        # User Types "1: go shopping" into the prompt
+        inputbox.send_keys("Go shopping")
+        # User hits enter, the page updates and "1: go shopping" is an item on the lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Go shopping" for row in rows)
+        )
+        # A text box to add another item is still present
+        # User adds "2: Remember to pick up dry-cleaning" and enters
+        # Page updates again and both items appear on list
+        # Website generates a unique url for the list, it is explained
+        # Once URL is visted, the list is there
+        self.fail("Finish the test!")
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
