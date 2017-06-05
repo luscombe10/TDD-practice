4c53536c1291ab24400f3a88eaccb55f45a8d828
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-05 21:56:45 +0100

    explained QueryDict (POST/GET) attributes of HttpRequest in view

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home_page(request):
+    print(request.POST, "WAZZAP")
     return render(request,
                   "home.html",{
                       "new_item_text": request.POST.get("item_text", ""),
+                      # POST dictionary.get() return None if no match found by default, returns "" here
                   })
