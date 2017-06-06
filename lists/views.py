e8aa8b130593201a388a988e7db76ef4fb2522a0
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-06 21:54:44 +0100

    Model for list items and associated migrations created, basic model unit test passed

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home_page(request):
-    print(request.POST, "WAZZAP")
     return render(request,
                   "home.html",{
                       "new_item_text": request.POST.get("item_text", ""),
