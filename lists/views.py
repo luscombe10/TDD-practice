b6740f00985e54fbf8a7c6ae6be556c0ce26f52c
henry luscombe <henry luscombe>
2017-06-07 14:20:02 +0100

    Saved POST method to database, refactored view for list items

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Item
+# from django.http import HttpResponse
+
+
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get("item_text", "")
+    item.save()
     return render(request,
-                  "home.html",{
-                      "new_item_text": request.POST.get("item_text", ""),
-                      # POST dictionary.get() return None if no match found by default, returns "" here
-                  })
+                  "home.html", {
+                      "new_item_text": item.text})
