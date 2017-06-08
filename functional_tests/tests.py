a2b4cbd0d5770789dc47ee35629d2d526409bd24
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2017-06-08 20:57:50 +0100

    updated explicit wait in functional test, now is loop that breaks when correct input is detected

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
 
 import time
 
-
+MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+            time.sleep(0.5)
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -37,13 +47,13 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("Go shopping")
         # User hits enter, the page updates and "1: go shopping" is an item on the lists
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table("1: Go shopping")
         # A text box to add another item is still present
         # User adds "2: Remember to pick up dry-cleaning" and enters
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Go to bank")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table("2: Go to bank")
         # Page updates again and both items appear on list
         self.check_for_row_in_list_table("1: Go shopping")
         self.check_for_row_in_list_table("2: Go to bank")
